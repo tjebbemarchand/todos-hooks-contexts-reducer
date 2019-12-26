d842b12e3e1eeb3ed98f31c3bc46228fba63cc04
Tjebbe <tjebbemarchand@icloud.com>
2019-12-26 08:41:47 +0100

    Created TodoForm component with a custom hook for adding todos

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import TodoList from "./TodoList";
+import TodoForm from "./TodoForm";
 import Typography from "@material-ui/core/Typography";
 import Paper from "@material-ui/core/Paper";
 import AppBar from "@material-ui/core/AppBar";
@@ -13,6 +14,9 @@ function TodoApp() {
         { id: 3, task: "Grow Beard", completed: false }
     ];
     const [todos, setTodos] = useState(initialTodos);
+    const addTodo = newTodoText => {
+        setTodos([...todos, { id: 4, task: newTodoText, completed: false }]);
+    };
     return (
         <Paper
             style={{
@@ -27,6 +31,7 @@ function TodoApp() {
                     <Typography color="inherit">Todos with Hooks</Typography>
                 </Toolbar>
             </AppBar>
+            <TodoForm addTodo={addTodo} />
             <TodoList todos={todos} />
         </Paper>
     );
