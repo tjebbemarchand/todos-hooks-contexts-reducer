5a3aaeb7b6f0424ed17fc588e1e38c3d7c728813
Tjebbe <tjebbemarchand@icloud.com>
2019-12-27 10:41:59 +0100

    Save todos to localStorage

--- src/TodoList.js
+++ src/TodoList.js
@@ -5,26 +5,28 @@ import Divider from "@material-ui/core/Divider";
 import Todo from "./Todo";
 
 function TodoList({ todos, toggleTodo, editTodo, removeTodo }) {
-    return (
-        <Paper>
-            <List>
-                {todos.map((todo, i) => (
-                    <>
-                        <Todo
-                            key={todo.id}
-                            id={todo.id}
-                            task={todo.task}
-                            completed={todo.completed}
-                            toggleTodo={toggleTodo}
-                            editTodo={editTodo}
-                            removeTodo={removeTodo}
-                        />
-                        {i < todos.length - 1 && <Divider />}
-                    </>
-                ))}
-            </List>
-        </Paper>
-    );
+    if (todos.length)
+        return (
+            <Paper>
+                <List>
+                    {todos.map((todo, i) => (
+                        <>
+                            <Todo
+                                key={todo.id}
+                                id={todo.id}
+                                task={todo.task}
+                                completed={todo.completed}
+                                toggleTodo={toggleTodo}
+                                editTodo={editTodo}
+                                removeTodo={removeTodo}
+                            />
+                            {i < todos.length - 1 && <Divider />}
+                        </>
+                    ))}
+                </List>
+            </Paper>
+        );
+    return null;
 }
 
 export default TodoList;
