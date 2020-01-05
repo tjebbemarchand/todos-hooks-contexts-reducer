7edcad491d8961efcf6f892a563fd6c69622c3db
Tjebbe <tjebbemarchand@icloud.com>
2020-01-05 19:14:22 +0100

    Updates

---
+++ precache-manifest.a7293a02a591280b8fe04ed27bb7df1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a21ecb0048961928b7ee50f443c15ed",
+    "url": "/todos-hooks-contexts-reducer/index.html"
+  },
+  {
+    "revision": "6d759dc83f29d623ccb3",
+    "url": "/todos-hooks-contexts-reducer/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "782e8bbcce4247dca879",
+    "url": "/todos-hooks-contexts-reducer/static/js/2.1d9fe81c.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/todos-hooks-contexts-reducer/static/js/2.1d9fe81c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6d759dc83f29d623ccb3",
+    "url": "/todos-hooks-contexts-reducer/static/js/main.73ddd72c.chunk.js"
+  },
+  {
+    "revision": "c1355b136c464d10f038",
+    "url": "/todos-hooks-contexts-reducer/static/js/runtime-main.634a5428.js"
+  }
+]);
